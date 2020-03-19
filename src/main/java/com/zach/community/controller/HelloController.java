007bf9e382031dc870236c55febc2d84115f64c4
Zach <524547428@qq.com>
2020-03-19 22:41:21 +0800

    HelloWorld TEST

---
+++ src/main/java/com/zach/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zach.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zach - 吸柒
+ *
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
